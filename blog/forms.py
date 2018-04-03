d700263402790276f19e4585418f1e32f4212810
Arindam Roychowdhury <arindamroychowdhury@Arindams-MacBook-Pro.local>
2018-04-04 02:14:17 +0530

    Form created for the creat blog

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from . import models
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = models.Post
+        fields = ['title', 'text']
+
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,10 @@
 from __future__ import unicode_literals
 from django.utils import timezone
 from django.shortcuts import render
+from django.http import HttpResponse
 from . import models
+from . import forms
+
 
 # Create your views here.
 def post_list(request):
@@ -14,4 +17,12 @@ def post_details(request, post_pk):
     post = models.Post.objects.get(pk=post_pk)
     return render(request, 'blog/post_details.html', {'post': post })
 
-	
+def create_post(request):
+    form = forms.BlogPostForm()
+    if request.method == 'POST':
+        form = forms.BlogPostForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponse('Well done!')
+
+
