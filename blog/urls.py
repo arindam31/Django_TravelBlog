485f713c308b904f51fc0858a9f08a10dbe8013d
arindam31 <thearc31@gmail.com>
2018-04-03 19:16:21 +0530

    Base html added...TODO:COmplete the post details page and test it

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
+	url(r'^$', views.post_details, name='post_details'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,11 @@ from . import models
 # Create your views here.
 def post_list(request):
 	posts =models.Post.objects.filter(created_date__lte=timezone.now()).order_by('published_date')
-	print posts
 	return render(request, 'blog/post_list.html', {'posts':posts})
 
 
+def post_details(request, post_pk):
+	post = models.Post.objects.get(pk=post_pk)
+	return render(request, 'blog/post_details.html', {'post': post })
+
+	
