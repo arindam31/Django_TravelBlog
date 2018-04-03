69e2047e9df988d06007aab73b1b40cf5064a35c
Arindam Roychowdhury <arindamroychowdhury@Arindams-MacBook-Pro.local>
2018-04-04 01:49:44 +0530

    Post details work fine

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
-	url(r'^$', views.post_details, name='post_details'),
+	url(r'(?P<post_pk>\d+)/$', views.post_details, name='post_details'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -6,12 +6,12 @@ from . import models
 
 # Create your views here.
 def post_list(request):
-	posts =models.Post.objects.filter(created_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html', {'posts':posts})
+    posts =models.Post.objects.filter(created_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
 
 
 def post_details(request, post_pk):
-	post = models.Post.objects.get(pk=post_pk)
-	return render(request, 'blog/post_details.html', {'post': post })
+    post = models.Post.objects.get(pk=post_pk)
+    return render(request, 'blog/post_details.html', {'post': post })
 
 	
