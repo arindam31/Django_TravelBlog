f8a30e3279c6c4c7ba6133226a6378ba1a82678c
Arindam Roychowdhury <arindam31@yahoo.co.in>
2018-04-13 01:57:55 +0000

    Removed tinymce reference from migration file

--- blog/migrations/0002_auto_20180404_1240.py
+++ blog/migrations/0002_auto_20180404_1240.py
@@ -3,19 +3,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
-import tinymce.models
 
 
 class Migration(migrations.Migration):
-
-    dependencies = [
-        ('blog', '0001_initial'),
-    ]
-
-    operations = [
-        migrations.AlterField(
-            model_name='post',
-            name='text',
-            field=tinymce.models.HTMLField(),
-        ),
-    ]
+    pass
