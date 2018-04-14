35998d81bf3428b9b4dc3d5d009c142d687abc7f
Arindam Roychowdhury <arindamroychowdhury@Arindams-MacBook-Pro.local>
2018-04-14 12:48:05 +0530

    API for creating Post(generic view)

--- blog/api.py
+++ blog/api.py
@@ -1,3 +1,5 @@
+from rest_framework import generics
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import serializers
@@ -8,3 +10,13 @@ class ListPostApi(APIView):
         posts = models.Post.objects.all()
         serializer = serializers.PostSerializers(posts, many=True) #If its a query list, many = True
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = serializers.PostSerializers(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class PostCreateApi(generics.ListCreateAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializers
--- blog/api_url.py
+++ blog/api_url.py
@@ -3,5 +3,5 @@ from . import views
 from . import api
 
 urlpatterns = [
-        url(r'^$', api.ListPostApi.as_view(), name='api_course_list'),
+        url(r'^$', api.PostCreateApi.as_view(), name='api_course_list'),
         ]
