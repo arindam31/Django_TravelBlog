ad5f61db931e128fbe038c14246d1285a39de387
arindam31 <thearc31@gmail.com>
2018-05-07 14:25:20 +0530

    Added filters and more in admin for Post model

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from . import models
 
+class PostAdmin(admin.ModelAdmin):
+	search_fields = ['title', ]
+	list_display = ['title', 'created_date', 'favourite', 'likes', 'published']
+	list_editable = ['favourite', 'published']
+
 # Register your models here.
-admin.site.register(models.Post)
+admin.site.register(models.Post, PostAdmin)
 admin.site.register(models.Tag)
 admin.site.register(models.Comment)
