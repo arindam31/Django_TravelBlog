a297beb04b5cb7b2a72cc1c3707542dca59f7a48
arindam31 <thearc31@gmail.com>
2018-05-18 13:20:09 +0530

    Debug False

--- Django_TravelBlog/settings.py
+++ Django_TravelBlog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8ht8yamzqd3%u1yztx_xe88a!*528&j+1zmy((a))cr=9o7-0^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '172.29.169.103', 'localhost']
 
