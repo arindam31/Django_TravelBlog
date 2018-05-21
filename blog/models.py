86d54ad946fcc93d826ea776a5be026a99828ab8
arindam31 <thearc31@gmail.com>
2018-05-21 13:49:34 +0530

    New field short text added for Post

--- blog/models.py
+++ blog/models.py
@@ -19,6 +19,7 @@ class Post(models.Model):
 	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
 	text = RichTextField()
+	short_intro = models.TextField(default='')
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
 	published = models.BooleanField(default=False)
