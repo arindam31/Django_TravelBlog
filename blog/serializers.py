2ca5daca49955b73820aca9a142178769f0f8802
arindam31 <thearc31@gmail.com>
2018-06-21 10:39:50 +0530

    POST api now shows name and id of tag

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from . import models
 
 class PostSerializers(serializers.ModelSerializer):
+
     class Meta:
     	model = models.Post
-    	fields = ('title', 'created_date', 'published_date', 'likes', 'favourite', 'tags', 'published')
+        depth = 1
+    	fields = ('title', 'pk', 'created_date', 'published_date', 'likes', 'favourite', 'tags', 'published')
 
 class CommentSerializers(serializers.ModelSerializer):
     class Meta:
